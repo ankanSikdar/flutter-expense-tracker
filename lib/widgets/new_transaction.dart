4790149ff77e30f90fd596084fa439952800f28b
Ankan Sikdar <ankansikdar@gmail.com>
2020-08-29 21:31:36 +0530

    Added DatePicker

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class NewTransaction extends StatefulWidget {
   final Function addTransaction;
@@ -12,6 +13,8 @@ class NewTransaction extends StatefulWidget {
 class _NewTransactionState extends State<NewTransaction> {
   final titleController = TextEditingController();
   final amountController = TextEditingController();
+  final dateController = TextEditingController();
+  DateTime pickedDate;
 
   void onSubmit() {
     String title = titleController.text;
@@ -28,12 +31,32 @@ class _NewTransactionState extends State<NewTransaction> {
     Navigator.pop(context);
   }
 
+  void startDatePicker() {
+    showDatePicker(
+            context: context,
+            initialDate: DateTime.now(),
+            firstDate: DateTime.parse("2020-01-01 00:00:01Z"),
+            lastDate: DateTime.now())
+        .then((value) {
+      if (value == null) {
+        return;
+      }
+      pickedDate = value;
+      dateController.text = DateFormat.yMMMd().format(pickedDate);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
       margin: EdgeInsets.symmetric(
         horizontal: 10,
         vertical: 20,
+      ),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(
+          18,
+        ),
       ),
       elevation: 8,
       child: Container(
@@ -54,13 +77,50 @@ class _NewTransactionState extends State<NewTransaction> {
               controller: amountController,
               onSubmitted: (_) => onSubmit(),
             ),
+            Container(
+              margin: EdgeInsets.only(
+                top: 10,
+                bottom: 30,
+              ),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      readOnly: true,
+                      controller: dateController,
+                      decoration: InputDecoration(labelText: 'Date'),
+                      enabled: false,
+                    ),
+                  ),
+                  FlatButton(
+                    onPressed: startDatePicker,
+                    child: Text(
+                      'Choose Date',
+                      style: TextStyle(
+                        color: Colors.purple,
+                        fontSize: 16,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
             FlatButton(
               onPressed: onSubmit,
               child: Text(
                 'Add Transaction',
                 style: TextStyle(
-                  color: Colors.purple,
+                  color: Colors.white,
+                  fontSize: 18,
                 ),
+              ),
+              color: Colors.purple,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(18),
+              ),
+              padding: EdgeInsets.symmetric(
+                horizontal: 20,
+                vertical: 10,
               ),
             )
           ],
