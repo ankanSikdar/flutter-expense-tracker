ec5072ac00290da77aee40dfdf9cf330f1fe468b
Ankan Sikdar <ankansikdar@gmail.com>
2021-09-22 22:04:41 +0530

    Added likk_picker to pick bill image

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,6 +1,9 @@
+import 'dart:io';
+
 import 'package:expense_app/extensions/currency_extension.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:likk_picker/likk_picker.dart';
 
 class NewTransaction extends StatefulWidget {
   final Function addTransaction;
@@ -17,6 +20,41 @@ class _NewTransactionState extends State<NewTransaction> {
   final dateController = TextEditingController();
   final formKey = GlobalKey<FormState>();
   DateTime pickedDate;
+  File imageFile;
+
+  GalleryController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    setState(() {
+      controller = GalleryController(
+        gallerySetting: GallerySetting(
+          enableCamera: true,
+          maximum: 1,
+          requestType: RequestType.image,
+          onItemClick: (entity, list) async {
+            if (list.isNotEmpty) {
+              final file = await list[0].entity.file;
+              updateImage(file);
+              Navigator.pop(context);
+            }
+          },
+        ),
+      );
+    });
+  }
+
+  void updateImage(File image) {
+    final fileExtension = image.path.split('.').last;
+    if (fileExtension == 'jpg' ||
+        fileExtension == 'jpeg' ||
+        fileExtension == 'png') {
+      setState(() {
+        imageFile = image;
+      });
+    }
+  }
 
   void onSubmit() {
     if (!formKey.currentState.validate()) {
@@ -44,6 +82,12 @@ class _NewTransactionState extends State<NewTransaction> {
       pickedDate = value;
       dateController.text = DateFormat.yMMMd().format(pickedDate);
     });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
   }
 
   @override
@@ -141,23 +185,44 @@ class _NewTransactionState extends State<NewTransaction> {
                     ],
                   ),
                 ),
-                FlatButton(
-                  onPressed: onSubmit,
-                  child: Text(
-                    'Add Transaction',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 18,
+                Row(
+                  children: [
+                    IconButton(
+                      onPressed: () async {
+                        final entity = await controller.pick(context);
+                        if (entity.isNotEmpty) {
+                          final file = await entity[0].entity.file;
+                          updateImage(file);
+                        }
+                      },
+                      icon: Icon(Icons.image),
                     ),
-                  ),
-                  color: Colors.purple,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(18),
-                  ),
-                  padding: EdgeInsets.symmetric(
-                    horizontal: 20,
-                    vertical: 10,
-                  ),
+                    if (imageFile != null)
+                      Container(
+                        height: 50,
+                        width: 50,
+                        child: Image.file(imageFile),
+                      ),
+                    Spacer(),
+                    FlatButton(
+                      onPressed: onSubmit,
+                      child: Text(
+                        'Add Transaction',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                        ),
+                      ),
+                      color: Colors.purple,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18),
+                      ),
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 20,
+                        vertical: 10,
+                      ),
+                    ),
+                  ],
                 ),
                 SizedBox(
                   height: 10,
