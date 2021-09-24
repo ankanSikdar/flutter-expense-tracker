c0702e0df9dc4cd1b4e32e262ece9dbb4c309835
Ankan Sikdar <ankansikdar@gmail.com>
2021-09-24 21:05:33 +0530

    Designed the details screen

--- lib/screens/details.dart
+++ lib/screens/details.dart
@@ -20,35 +20,22 @@ class DetailsPage extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              SizedBox(
-                width: double.infinity,
-              ),
-              Text(
-                _transaction.title,
-                style: TextStyle(
-                  fontSize: 28.0,
-                  color: Theme.of(context).primaryColor,
-                  fontWeight: FontWeight.bold,
-                ),
-                textAlign: TextAlign.justify,
-              ),
-              SizedBox(height: 32.0),
-              Text(
-                '${getCurrencySymbol()} ${_transaction.amount.toStringAsFixed(2)}',
-                style: TextStyle(fontSize: 22.0),
-              ),
-              SizedBox(height: 16.0),
-              Text(
-                'Date: ${DateFormat.yMMMMEEEEd().format(_transaction.date)}',
-              ),
-              Text(
-                  'Created At: ${DateFormat.yMMMEd().format(_transaction.createdOn)} ${DateFormat.jm().format(_transaction.createdOn)}'),
-              SizedBox(height: 16.0),
+              DetailsRow(title: 'Title:', content: _transaction.title),
+              DetailsRow(
+                  title: 'Amount:',
+                  content:
+                      '${getCurrencySymbol()} ${_transaction.amount.toStringAsFixed(2)}'),
+              DetailsRow(
+                  title: 'Date:',
+                  content:
+                      '${DateFormat.yMMMMEEEEd().format(_transaction.date)}'),
+              DetailsRow(
+                  title: 'Added:',
+                  content:
+                      '${DateFormat.yMMMEd().format(_transaction.createdOn)} ${DateFormat.jm().format(_transaction.createdOn)}'),
               if (_transaction.imagePath.isNotEmpty)
                 Container(
-                  height: 200,
                   width: double.infinity,
                   child: Image.file(
                     File(_transaction.imagePath),
@@ -58,6 +45,48 @@ class DetailsPage extends StatelessWidget {
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class DetailsRow extends StatelessWidget {
+  final String title;
+  final String content;
+
+  const DetailsRow({
+    Key key,
+    @required this.title,
+    @required this.content,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 16.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width * 0.25,
+            margin: EdgeInsets.only(right: 8.0),
+            child: Text(
+              title,
+              style: TextStyle(
+                  color: Theme.of(context).primaryColorDark,
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+          Expanded(
+            child: Text(
+              content,
+              style: TextStyle(
+                fontSize: 16.0,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
